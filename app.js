33f98abb8f4bd5f7a34453b05c6614fb38af898d
Anyalee610 <anayalambright610@gmail.com>
2023-02-13 23:21:53 -0500

    login check

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const jobPostRoutes = require('./routes/job_postRoutes');
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5019;
+const port = process.env.PORT || 5011;
 app.use(cors());
 app.use(express.json());
 
